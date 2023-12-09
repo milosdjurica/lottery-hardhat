83212d0322151adef766ecc0afad207bf7fe4a0f
milosdjurica <milosdjurica13@gmail.com>
2023-12-09 21:11:21 +0100

    added tests for entering lottery without money, with not enough money and test for adding player to array after entering with money

--- test/unit/Lottery.test.ts
+++ test/unit/Lottery.test.ts
@@ -1,23 +1,46 @@
 import { deployments, ethers, getNamedAccounts } from "hardhat";
 import { Lottery } from "../../typechain-types";
-import { assert } from "chai";
+import { assert, expect } from "chai";
 
 describe("Lottery", () => {
 	let lottery: Lottery;
 	let deployer: string;
-	const ticketPrice = 20;
+	const TICKET_PRICE = 20;
 
 	beforeEach(async () => {
 		await deployments.fixture(["all"]);
 		deployer = (await getNamedAccounts()).deployer;
 		lottery = await ethers.getContract("Lottery", deployer);
-		console.log("lottery", lottery);
+		// console.log("lottery", lottery);
 	});
 
 	describe("constructor", () => {
 		it("sets ticket price correctly", async () => {
 			const response = await lottery.getTicketPrice();
-			assert.equal(ticketPrice, Number(response));
+			assert.equal(TICKET_PRICE, Number(response));
+		});
+	});
+
+	describe("enterLottery", () => {
+		it("reverts if called without money", async () => {
+			await expect(lottery.enterLottery()).to.be.revertedWithCustomError(
+				lottery,
+				"Lottery__NotEnoughETH",
+			);
+		});
+		it("reverts if not enough money", async () => {
+			await expect(
+				lottery.enterLottery({ value: TICKET_PRICE - 1 }),
+			).to.be.revertedWithCustomError(lottery, "Lottery__NotEnoughETH");
+		});
+
+		it("adds to array of players", async () => {
+			const accounts = await ethers.getSigners();
+			const lotteryConnected = lottery.connect(accounts[1]);
+			await lotteryConnected.enterLottery({
+				value: TICKET_PRICE,
+			});
+			assert.equal(accounts[1].address, await lottery.getPlayer(0));
 		});
 	});
 });
