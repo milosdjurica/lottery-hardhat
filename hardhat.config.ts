a5d8e657185684dc6481f6f5b68369757dcd68bd
milosdjurica <milosdjurica13@gmail.com>
2023-12-09 19:05:45 +0100

    added prettier and solhint files, and added .env variables into hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,14 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+import "hardhat-deploy";
+import "hardhat-deploy-ethers";
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_PROCESS_ENV || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xKEY";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "api-key";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "api-key";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
