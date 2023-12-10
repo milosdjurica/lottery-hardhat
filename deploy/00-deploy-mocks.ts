028ba70c4975760ccff81baf9b3d92cdf5cf6a7d
milosdjurica <milosdjurica13@gmail.com>
2023-12-10 01:29:25 +0100

    deploy mocks script finished

--- deploy/00-deploy-mocks.ts
+++ deploy/00-deploy-mocks.ts
@@ -1,16 +1,28 @@
 import { DeployFunction } from "hardhat-deploy/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { developmentChains } from "../hardhat.config";
-import { network } from "hardhat";
+import { ethers, network } from "hardhat";
+
+const BASE_FEE = ethers.parseEther("0.25"); // paying 0.25 LINK every time random numbers are requested
+const GAS_PRICE_LINK = 1e9; // calculated value based on the gas price of the chain
 
 const deployLottery: DeployFunction = async function (
 	hre: HardhatRuntimeEnvironment,
 ) {
-	const { deployer, log } = await hre.getNamedAccounts();
-	const { deploy } = hre.deployments;
+	const { deployer } = await hre.getNamedAccounts();
+	const { deploy, log } = hre.deployments;
 
 	if (developmentChains.includes(network.name)) {
 		console.log("Local network detected! Deploying mocks...");
+
+		await deploy("VRFCoordinatorV2Mock", {
+			from: deployer,
+			args: [BASE_FEE, GAS_PRICE_LINK],
+			log: true,
+		});
+
+		log("Mocks deployed!!!");
+		log("===============================================================");
 	}
 
 	const lottery = await deploy("Lottery", {
